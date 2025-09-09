9d156fa83dd0ff8111235949023a8da949e80eaa
netflypsb <netflypsb@gmail.com>
2025-09-09 14:08:39 +0800

    v1.2: overcame local development error. deploying to vercel to see if any deployment issues. similar to v2.0 (but 2.0 has deployment issues) and 1.2 (but 1.2 has local errors)

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  transpilePackages: [
+    '@radix-ui/react-dialog',
+    '@radix-ui/react-dismissable-layer',
+    '@radix-ui/react-menu',
+    '@radix-ui/react-select',
+    '@radix-ui/react-tooltip',
+    '@radix-ui/react-visually-hidden'
+  ],
+};
+
+export default nextConfig;
